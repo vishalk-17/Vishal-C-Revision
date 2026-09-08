7c50f1d0f490a8aae57e4443a754147d84e8d429
vishalk-17 <vk0262401@gmail.com>
2026-09-08 19:56:14 +0530

    simply

--- Leetcode Project/Day06/src/index.js
+++ Leetcode Project/Day06/src/index.js
@@ -11,7 +11,7 @@ const problemRouter = require("./routes/problemCreator");
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/auth',authRouter);
+app.use('/user',authRouter);
 app.use('/problem',problemRouter);
 
 
--- Leetcode Project/Day06/src/routes/problemCreator.js
+++ Leetcode Project/Day06/src/routes/problemCreator.js
@@ -1,19 +1,20 @@
 const express = require('express');
 
 const problemRouter =  express.Router();
-const adminMiddleware = require("../middleware/adminMiddleware")
-const createProblem = require("../controllers/userProblem");
+const adminMiddleware = require("../middleware/adminMiddleware");
+const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem} = require("../controllers/userProblem");
+const userMiddleware = require("../middleware/userMiddleware");
 
 
 // Create
 problemRouter.post("/create",adminMiddleware ,createProblem);
-// problemRouter.patch("/:id", updateProblem);
-// problemRouter.delete("/:id",deleteProblem);
+problemRouter.put("/update/:id",adminMiddleware, updateProblem);
+problemRouter.delete("/delete/:id",adminMiddleware, deleteProblem);
 
 
-// problemRouter.get("/:id",getProblemById);
-// problemRouter.get("/", getAllProblem);
-// problemRouter.get("/user", solvedAllProblembyUser);
+problemRouter.get("/problemById/:id",userMiddleware,getProblemById);
+problemRouter.get("/getAllProblem",userMiddleware, getAllProblem);
+
 
 
 module.exports = problemRouter;
@@ -21,4 +22,3 @@ module.exports = problemRouter;
 // fetch
 // update
 // delete 
-
